adbc7735e5141943db40419005ea1c37202798fc
Hung Nguyen <hungnv0607.dev@gmail.com>
2019-01-12 11:49:44 +0700

    Update layout sidebar

---
+++ wp-content/themes/unicase-child/templates/layouts/layout-sidebar.php
@@ -0,0 +1,26 @@
+<?php
+	
+$default_args = array(
+	'main_content'		=> '',
+	'sidebar_action'	=> 'unicase_sidebar',
+);
+
+if( !empty( $args ) ) {
+	$args = array_merge( $default_args, $args );
+}
+
+extract( $args );
+?>
+
+<div class="<?php echo esc_attr( apply_filters( 'unicase_container_classes', 'container' ) ); ?>">
+	<div class="row">
+        <aside id="secondary" class="<?php echo esc_attr( apply_filters( 'unicase_sidebar_area_classes', 'sidebar-area widget-area' ) ); ?>">
+            <?php do_action( $sidebar_action ); ?>
+        </aside>
+		<div id="primary" class="<?php echo esc_attr( apply_filters( 'unicase_content_area_classes', 'content-area' ) ); ?>">
+			<main class="<?php echo esc_attr( apply_filters( 'unicase_site_main_classes', 'site-main' ) ); ?>">
+				<?php echo $main_content; ?>
+			</main>
+		</div>
+	</div>
+</div>
