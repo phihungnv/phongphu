220f8454e986e79efd73d1bd4fec4c1eddf64b40
Hung Nguyen <hungnv0607.dev@gmail.com>
2019-05-08 22:17:29 +0700

    Update homepage

---
+++ wp-content/themes/unicase-child/sidebar-page.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The sidebar containing the page sidebar widget area
+ *
+ * @package Unicase
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+?>
+
+<aside class="sidebar sidebar-page">
+	<?php
+    if (is_front_page()) {
+        dynamic_sidebar( 'homepage-sidebar' );
+    } else if ( ! is_active_sidebar( 'page-sidebar-1' ) ) {
+		if ( function_exists( 'unicase_default_homepage_widgets' ) ) {
+			unicase_default_homepage_widgets();
+		}
+	} else {
+		dynamic_sidebar( 'page-sidebar-1' );
+	}
+	?>
+</aside><!-- /.sidebar -->
